30594854a5325cb86ead633e31c9b62d4aa7ee8b
FanidiyaTasya <fanidiyatasya@gmail.com>
2024-10-18 11:06:02 +0700

    feat: add controller for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $guarded = ['id'];
+    public $timestamps = false;
+
     /**
      * The attributes that are mass assignable.
      *
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
-    return view('pages.login');
-});
+Route::get('/', [AuthController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('pages.dashboard');

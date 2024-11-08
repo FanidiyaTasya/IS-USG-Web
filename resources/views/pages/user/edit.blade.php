475111df5df037767885a78013e7ce0cc82ff8b0
FanidiyaTasya <fanidiyatasya@gmail.com>
2024-11-08 12:45:23 +0700

    feat: add create, update, and delete functionality for user data

--- resources/views/pages/user/edit.blade.php
+++ resources/views/pages/user/edit.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.master')
 
 @section('content')
-<form action="{{ route('user.store') }}" method="POST">
+<form action="{{ route('user.update', $user->id) }}" method="POST">
   @csrf
+  @method('PUT')
   <div class="col-12">
     <div class="card">
       <div class="px-4 py-3 border-bottom">
@@ -13,7 +14,7 @@
           <div class="col-lg-6">
             <div class="mb-4">
               <label for="name" class="form-label">Nama</label>
-              <input type="text" id="name" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}">
+              <input type="text" id="name" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $user->name) }}">
               @error('name')
               <div class="invalid-feedback">
                   {{ $message }}
@@ -24,7 +25,7 @@
             <div class="mb-4">
               <label for="email" class="form-label">Email</label>
               <div class="input-group">
-                <input type="text" name="email" id="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}">
+                <input type="text" name="email" id="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email', $user->email) }}" readonly>
                 @error('email')
                 <div class="invalid-feedback d-block">
                     {{ $message }}
@@ -37,8 +38,8 @@
               <label for="role" class="form-label">Role</label>
               <select class="form-select @error('role') is-invalid @enderror" name="role" id="role" aria-label="Default select example">
                 <option selected disabled>Pilih Role</option>
-                <option value="Admin" {{ old('role') == 'Admin' ? 'selected' : '' }}>Admin</option>
-                <option value="Karyawan" {{ old('role') == 'Karyawan' ? 'selected' : '' }}>Karyawan</option>
+                <option value="Admin" {{ old('role', $user->role) == 'Admin' ? 'selected' : '' }}>Admin</option>
+                <option value="Karyawan" {{ old('role', $user->role) == 'Karyawan' ? 'selected' : '' }}>Karyawan</option>
               </select>
               @error('role')
               <div class="invalid-feedback">
@@ -50,7 +51,7 @@
   
           <div class="col-lg-6">
             <div class="mb-4">
-              <label for="password" class="form-label">Password</label>
+              <label for="password" class="form-label">Password (Kosongkan jika tidak ingin mengubah)</label>
               <div class="input-group">
                 <input type="password" id="password" name="password" class="form-control @error('password') is-invalid @enderror">
                 @error('password')
@@ -75,10 +76,10 @@
           </div>
         </div>
       </div>
-      <div class="card-bo dy p-4">
+      <div class="card-body p-4">
           <div class="col-12">
               <div class="d-flex align-items-center gap-3">
-                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="submit" class="btn btn-primary">Update</button>
               </div>
           </div>
       </div>

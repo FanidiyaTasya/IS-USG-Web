4a5f7ef649f229c4192138a3527a3e2f5a95e355
FanidiyaTasya <fanidiyatasya@gmail.com>
2024-11-25 12:23:22 +0700

    fix: fix radiology request

--- app/Http/Requests/RadiologyRequest.php
+++ app/Http/Requests/RadiologyRequest.php
@@ -21,7 +21,7 @@ class RadiologyRequest extends FormRequest {
         $rules = [
             'ultrasound_image' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
             // 'gestational_age' => 'nullable|integer|min:0',
-            'pregnancy_status' => 'required|in:Hamil,Tidak Hamil',
+            'pregnancy_status' => 'required|in:Bunting,Tidak Bunting',
             'additional_info' => 'nullable|string'
         ];
 
@@ -40,7 +40,7 @@ class RadiologyRequest extends FormRequest {
             'ultrasound_image.max' => 'Ukuran gambar ultrasound tidak boleh lebih dari 2MB.',
             // 'gestational_age.integer' => 'Usia Kandungan harus berupa angka bulat.',
             'pregnancy_status.required' => 'Status kehamilan harus dipilih.',
-            'pregnancy_status.in' => 'Status kehamilan harus salah satu dari: Hamil, Tidak Hamil.',
+            'pregnancy_status.in' => 'Status kehamilan harus salah satu dari: Bunting, Tidak Bunting.',
         ];
     }
 }
